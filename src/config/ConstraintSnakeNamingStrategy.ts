f03d9830c2017c879cfc5009fb668e3980f43ff6
jojoldu <jojoldu@gmail.com>
2021-05-30 15:41:44 +0900

    fk constraints name rule 교체

--- src/config/ConstraintSnakeNamingStrategy.ts
+++ src/config/ConstraintSnakeNamingStrategy.ts
@@ -7,10 +7,15 @@ export class ConstraintSnakeNamingStrategy extends SnakeNamingStrategy {
         super();
     }
 
-    primaryKeyName(tableOrName: Table | string, columnNames: string[]) {
+    primaryKeyName(tableOrName: Table | string, columnNames: string[]): string {
         const table = tableOrName instanceof Table ? tableOrName.name : tableOrName;
         const columnsSnakeCase = columnNames.join("_");
 
         return `pk_${table}_${columnsSnakeCase}`;
+    }
+
+    foreignKeyName(tableOrName: Table | string, columnNames: string[], _referencedTablePath?: string, _referencedColumnNames?: string[]): string {
+        const table = tableOrName instanceof Table ? tableOrName.name : tableOrName;
+        return `fk_${table}_${_referencedTablePath}`;
     }
 }
