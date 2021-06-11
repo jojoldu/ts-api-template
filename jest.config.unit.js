b70b683e0ca45fd88a6e4916ab3aa464d49be28f
jojoldu <jojoldu@gmail.com>
2021-06-11 09:24:25 +0900

    IDE에서 jest.config.ts를 unit과 intergration 구분을 못해서 unit만 별도 config로 분리
    * integration을 분리 안한 이유는 **단일 테스트 실행이 필요한건 오래 걸리는 integration**이기 때문이고
    * unit은 전체를 수행한다 해도 전체 시간이 크게 소요되지 않으니 차라리 unit을 CLI로 전체 수행하는게 더 낫다.

---
+++ jest.config.unit.js
@@ -0,0 +1,23 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testPathIgnorePatterns: [
+        "<rootDir>/node_modules/"
+    ],
+    testMatch: [
+        '<rootDir>/test/unit/**/*.test.ts',
+        '<rootDir>/test/config/**/*.test.ts',
+    ],
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/app.ts',
+        '!src/start.ts',
+        '!src/**/*.config.ts',
+        '!src/**/entity/**',
+        '!src/**/exception/**',
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+};
--- test/setup.ts
+++ test/setup.ts
@@ -3,9 +3,11 @@ import testConnection from "./integration/testConnection";
 process.env.NODE_ENV = "test";
 
 beforeAll(async () => {
+    console.log(`currentPath = ${process.cwd()}`)
     await testConnection.create();
 });
 
 afterAll(async () => {
+    console.log(`currentPath = ${process.cwd()}`)
     await testConnection.close();
 });
